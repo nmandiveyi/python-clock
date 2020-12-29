fe9ed52448bddf7d302e25a3725c7a1203b28546
Ngonindzashe Mandiveyi <ngonidzashehh@gmail.com>
2020-12-28 23:13:49 -0800

    time function

--- clock.py
+++ clock.py
@@ -7,6 +7,13 @@ from tkinter.ttk import *
 # Now import the time module
 from time import strftime
 
+def get_time():
+    time_stamp = strftime('%H:%M:%S %p')
+    label.config(text=time_stamp)
+    label.after(1000, get_time)
+
 # start the root window for the clock
 root = Tk()
+label = Label(root, font=('ds-digital', 100), bg='black',fg='white')
+label.pack(ANCHOR="center")
 
