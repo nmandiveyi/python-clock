25ea773cf00cd63b327dd2dc51666b615efbc4ac
Ngonindzashe Mandiveyi <ngonidzashehh@gmail.com>
2020-12-28 22:57:00 -0800

    initial stuff

---
+++ clock.py
@@ -0,0 +1,12 @@
+# This is a simple algorithm for running a clock
+
+# Import tkinter module for the gui
+from tkinter import *
+from tkinter.ttk import *
+
+# Now import the time module
+from time import strftime
+
+# start the root window for the clock
+root = Tk()
+
